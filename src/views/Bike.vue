9460fbd48dc7eeb2bc7a643173bda134e9b04d49
Vezona <vezona5tv@gmail.com>
2021-11-17 00:42:16 +0800

    載入leaflet

--- src/views/Bike.vue
+++ src/views/Bike.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class='max-w-1280px m-auto'>腳踏車</div>
+  <div class='max-w-1280px m-auto'>腳踏車
+  </div>
+  <div id="myMap h-full z-10"></div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue';
+import { ref, reactive, onMounted } from 'vue';
 import { getAuthorizationHeader } from '../js/getAuthorizationHeader';
+import leaflet from 'leaflet';
 export default {
   props: {
     // user: {
@@ -12,7 +15,21 @@ export default {
     //   required: true
     // }
   },
-  setup(props) {
+  setup() {
+
+      let myMap;
+      onMounted(()=>{
+        console.log(myMap);
+        myMap = leaflet.map("myMap", {
+            center: [22.595153, 120.306923],
+            zoom: 17
+            });
+    //     leaflet
+    //       .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    //   attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(mymap);
+
+      })
+
 
     // const data = ref([]);
     // const fetchData = async (url) => {
