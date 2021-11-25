91c7b1ad7c8c51890385fb5629ff77b470fcb3f8
vezonaChin <vezona5tv@gmail.com>
2021-11-25 19:56:38 +0800

    抓縣市區域的youbike 站點

---
+++ src/components/youbikeSearch.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="box absolute top-0 left-0 z-1000 rounded-sm w-1/3"
+       m="5"
+       p="2"
+       bg="white"
+       border="2 dark_green">
+    <!-- input -->
+    <div class="searchInput flex items-center"
+         bg="warm-gray-200"
+         p="y-1 x-2">
+      <input type="text" name="" id="" 
+           class="w-full text-sm"
+           bg="warm-gray-200" 
+           focus-visible="outline-none"
+           placeholder="搜尋地區或站點">
+      <icon-ic-baseline-search class="cursor-pointer"
+         hover="text-dark_green">
+      </icon-ic-baseline-search>
+    </div>
+
+    <!-- 搜縣市 -->
+    <div class="searchAreas p-2">
+      <div class="county flex justify-between items-center mb-2">  
+        <p class="">選擇縣市</p>
+        <select name="" id="" class="w-1/2 border-3" v-model="currCounty">
+          <option v-for="city in areaList" :key="city.id" :value="city">{{city.name}}</option>
+        </select>
+      </div>
+      <div class="district flex justify-between items-center">  
+        <p class="">選擇縣市</p>
+        <select name="" id="" class="w-1/2 border-3" v-model="currDistrict">
+        <option v-for="district in districtList" :key="district.id" :value="district">{{district.name}}</option>
+        </select>
+      </div>
+    </div>
+
+   </div>
+</template>
+
+<script>
+import { ref, reactive, computed, inject } from 'vue';
+import { getAuthorizationHeader } from '../js/getAuthorizationHeader';
+export default {
+  props: {
+    // user: {
+    //   type: String,
+    //   required: true
+    // }
+  },
+  setup(props, { emit }) {
+    // 縣市區域
+    const currCounty = inject('currCounty');
+    const currDistrict = inject('currDistrict') 
+
+   // 取縣市資料
+    const areaList = ref([]);
+    const fetchData = async () => {
+      const res = await fetch('https://raw.githubusercontent.com/kurotanshi/mask-map/master/raw/area-location.json')
+      const data = await res.json();
+      areaList.value = data
+      // 初始化地區
+      currCounty.value = data[0]
+      currDistrict.value = data[0].districts[0]
+    }
+
+    fetchData()
+
+    // 選擇的縣市與區域
+    const districtList = computed(() => {
+        return currCounty.value["districts"]
+    });
+
+
+    return {
+      areaList,
+      currCounty,
+      districtList,
+      currDistrict
+    } 
+  }
+}
+</script>
+
+ <style lang='scss' scoped>
+ 
+ </style>
--- src/views/YoubikeSpots.vue
+++ src/views/YoubikeSpots.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="myMap" class="min-h-screen-md z-10 m-auto"></div>
+  <div class="relative">
+    <!-- <div class="box absolute top-0 left-0 z-1000">搜尋器</div> -->
+    <youbike-search></youbike-search>
+    <div id="myMap" class="min-h-screen z-10 m-auto"></div>
+  </div>
 </template>
 
 <script>
-import { ref, reactive, onMounted } from 'vue';
+import { ref, reactive, onMounted, provide, watch } from 'vue';
 import { getAuthorizationHeader } from '../js/getAuthorizationHeader';
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
@@ -14,36 +18,56 @@ export default {
     //   required: true
     // }
   },
-  setup() {
-console.log(L);
+  setup(props) {
+    // 縣市區域
+    const currCounty = ref({});
+    provide('currCounty',currCounty);
 
-      let myMap;
-      onMounted(()=>{
-        myMap = L.map("myMap", {
-            center: [22.595153, 120.306923],
-            zoom: 17
-            });
-        L
-          .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(myMap);
+    const currDistrict = ref({});
+    provide('currDistrict',currDistrict);
 
-      })
+    const LatLng = reactive([])
+    let lat, lng;
+    watch(currDistrict, ()=>{
+      // console.log(currDistrict.value);
+      lat = currDistrict.value["latitude"];
+      lng = currDistrict.value["longitude"]
+      Object.assign(LatLng, [lat, lng])
+      myMap.flyTo(LatLng, 14)
+      // 打bike API
+      fetchData(`Bike/Station/NearBy?$spatialFilter=nearby(${lat},${lng},500)`);
+    })
+
+    // 地圖
+    let myMap;
+    onMounted(()=>{
+      myMap = L.map("myMap", {
+          center: [25.0263064, 121.5262934], // 高雄 22.595153, 120.306923
+          zoom: 6,
+          });
+      myMap.zoomControl.setPosition('bottomright');
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", 
+                    { attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>contributors'}
+                  ).addTo(myMap);
+
+    });
 
 
-    // const data = ref([]);
-    // const fetchData = async (url) => {
-    //   const res = await fetch(
-    //       `https://ptx.transportdata.tw/MOTC/v2/${url}`,
-    //       {
-    //       headers: getAuthorizationHeader()
-    //       }
-    //   )
-    //   const json = await res.json();
-    //   data.value = json;
-    //   console.log(data);
-    // }
-
-    // fetchData('Tourism/Restaurant?$top=6&$filter=contains(Description,'熱門') and Picture/PictureUrl1 ne null&$format=JSON')
+    const data = ref([]);
+    const fetchData = async (url) => {
+      const res = await fetch(
+          `https://ptx.transportdata.tw/MOTC/v2/${url}`,
+          {
+          headers: getAuthorizationHeader()
+          }
+      )
+      const json = await res.json();
+      data.value = json;
+      console.log(data);
+    }
+        // url: `Bike/Station/NearBy?$spatialFilter=nearby(${latitude},${longitude},500)`,
+    
 
     return {
     } 
@@ -52,4 +76,9 @@ console.log(L);
 </script>
 
  <style lang='scss' scoped>
+
+//  .leaflet-control-container { 
+//     position: absolute;
+//     right: 65px;
+//   } 
  </style>
